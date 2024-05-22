605f77baef1f61a67ec579a5e24620b0255aa3fb
Zoltan Erdos <zoltan.erdos@me.com>
2024-05-22 20:28:03 +0100

    fix: format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,34 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-
   const [input, setInput] = useState("");
-  const [{result, duration}, set] = useState({result: [""], duration: 0} );
+  const [{ result, duration }, set] = useState({ result: [""], duration: 0 });
 
-useEffect(()=>{
-const fetchData = async () => {
-  if (!input) return;
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!input) return;
 
-  try{
-  const response = await fetch(`/api/search?q=${input}`);
-  const json = await response.json();
+      try {
+        const response = await fetch(`/api/search?q=${input}`);
+        const json = await response.json();
 
-  set(json);
-  } catch (error) {
-    console.error("Error fetching data", error);
-  
+        set(json);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
 
-  }
-
-}
-
-fetchData();
-
-},[input]);
+    fetchData();
+  }, [input]);
 
   return (
-<div>
-  <input value={input}  onChange={(e)=>setInput(e.target.value)} type="text"></input>
-</div>
+    <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+      >
+      </input>
+    </div>
   );
 }
